c652a8ec743f5a4cf04fb9aa3ade50de0b88ae05
milosmitrovic20 <milosmitrovic005@gmail.com>
2025-04-01 15:04:14 +0200

    feat: add email input field to checkout form and update order submission validation

--- submit_order.php
+++ submit_order.php
@@ -17,7 +17,7 @@ mysqli_set_charset($conn, "utf8mb4");
 $data = json_decode(file_get_contents("php://input"), true);
 
 // Check if required fields are present
-if (!isset($data['first_name'], $data['last_name'], $data['address'], $data['city'], $data['zip_code'], $data['phone'], $data['cartItems'])) {
+if (!isset($data['first_name'], $data['last_name'], $data['address'], $data['city'], $data['zip_code'], $data['phone'], $data['email'], $data['cartItems'])) {
     echo json_encode(['success' => false, 'error' => 'Nedostaju potrebni podaci']);
     exit;
 }
@@ -47,7 +47,7 @@ try {
         $data['city'],
         $data['zip_code'],
         $data['phone'],
-        "",
+        $data['email'],
         $ukupnaCena,
         $cenaDostave
     );
